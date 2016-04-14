5620b8d139a99749a4a51a16c0ca57527a03182c
Changshuo1993 <sc3919@columbia.edu>
2016-04-14 10:38:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var http = require('http');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var request = require("request");
+var endpoint = 'search-tweetsenti-orhm23zddf2fg4shzs2w6rfgga.us-east-1.es.amazonaws.com';
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
 var es = require('elasticsearch');
-var endpoint = 'search-tweetsenti-orhm23zddf2fg4shzs2w6rfgga.us-east-1.es.amazonaws.com';
 var client = new es.Client({host: endpoint});
+
+var bodyParser = require('body-parser');
+var request = require("request");
 
 app.use(express.static('public'));
 app.use(bodyParser.json({type: 'text/plain'}));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
 
 app.post('/', function (req, res) {
@@ -29,11 +29,15 @@ app.post('/', function (req, res) {
     else if (type === 'Notification') {
         var id = req.body.MessageId;
         var message = req.body.Message;
+        console.log(message);
         client.index({
             index: 'twittmap',
             type: 'tweets',
             id: id,
             body: message
+        });
+        io.emit('newcoming', {
+            tweet: JSON.parse(message)
         });
     }
 });
@@ -43,7 +47,6 @@ server.listen(80);
 io.on('connection', function (socket) {
     socket.on('clicked', function (data) {
         var key = data.key;
-        console.log(key);
         client.search({
             q: key,
             size: 1000
@@ -53,11 +56,10 @@ io.on('connection', function (socket) {
             for (var i = 0; i < hits.length; i++) {
                 result[i] = hits[i]._source;
             }
-            console.log(result);
-            var myObject = {
-                "tweet": result
-            };
-            socket.emit('toggle', myObject);
+            socket.emit('toggle', {
+                tweet: result
+            });
         });
     });
 });
+
